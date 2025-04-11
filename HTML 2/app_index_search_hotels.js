578655fb87b7987935424a9f2e6996b8355e82f0
sajabilal <sajabilal69@gmail.com>
2025-04-11 18:49:52 +0300

    ready ses and sqs with lambdas

--- HTML 2/app_index_search_hotels.js
+++ HTML 2/app_index_search_hotels.js
@@ -1,19 +1,63 @@
-async function reserve(){
-  try {response = await fetch("https://o7w1f1d806.execute-api.us-east-1.amazonaws.com/PROD/SNS",{method:"POST", headers: {
-      "Content-Type": "application/x-www-form-urlencoded"  // Tell API Gateway that we are sending form data
-  },body: "Action=Publish&TopicArn=arn:aws:sns:us-east-1:476959162071:HotelEmailSending&Message=to DECLINE: https://9owvfe3g25.execute-api.us-east-1.amazonaws.com/decline to ACCEPT: https://9owvfe3g25.execute-api.us-east-1.amazonaws.com/accept"})
-  data= await response.text();
-  console.log("Server Response:", data);
-  alert("Email Sent Successfully! Check your inbox.");}
- catch (error) {
+let hotelName="";
+async function confirm(event) {
+  console.log(hotelName);
+  event.preventDefault();
+  customerEmail = document.getElementById("email").value;//user email
+  if (hotelName === "Monet Garden Hotel"){
+    hotelEmail = "monetgardenhotelresevation@gmail.com";
+  }
+  if (hotelName === "cc"){
+    hotelEmail = "monetgardenhotelresevation@gmail.com";
+  }
+  if (hotelName === "holiday"){
+    hotelEmail = "monetgardenhotelresevation@gmail.com";
+  }
+
+  try {
+    response = await fetch(
+      `https://lhoeea8hkl.execute-api.us-east-1.amazonaws.com/prod/ses-to-hotels`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json", // Tell API Gateway that we are sending form data
+        },
+        body: JSON.stringify({
+          "toEmails": [hotelEmail.trim()],
+          "subject" : "reservation request",
+          "message" : `to DECLINE: https://lhoeea8hkl.execute-api.us-east-1.amazonaws.com/prod/decline-to-customer?email=${customerEmail}&hotel=${encodeURIComponent(hotelName)} to ACCEPT: https://lhoeea8hkl.execute-api.us-east-1.amazonaws.com/accept-to-customer?email=${customerEmail}&hotel=${encodeURIComponent(hotelName)}`
+        })
+      }
+    );
+    data = await response.text();
+    console.log("Server Response:", data);
+    alert("Email Sent Successfully to hotel!");
+  } catch (error) {
     // If something goes wrong, log the error in the console
     console.error("Error sending email:", error);
 
     // Show an alert to tell the user that sending the email failed
     alert("Failed to send email. Please check the console for details.");
-}}
+  }
+}
 
-    
+function reserve(event) {
+  hotelName = event.target.id;
+  console.log(hotelName);
+  document.getElementById("reservationForm").style.display = "block";
+  document.getElementById("Monet Garden Hotel").style.display = "none";
+  return hotelName; 
+}
+
 document
   .getElementById("Monet Garden Hotel")
   .addEventListener("click", reserve); //accessing the hotel search bar and adding a listener to the enter pressed
+
+document
+.getElementById("cc")
+.addEventListener("click", reserve); //accessing the hotel search bar and adding a listener to the enter pressed
+
+document
+  .getElementById("holiday")
+  .addEventListener("click", reserve); //accessing the hotel search bar and adding a listener to the enter pressed
+
+document.getElementById("reservationForm").addEventListener("submit", confirm);
